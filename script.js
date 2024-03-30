3e2944df44897944927b8f4c7f72aa44724dd120
AlexeyAndreevWeb <lexandrweb@gmail.com>
2024-03-30 18:50:47 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,78 @@
+// class Animal {
+//   constructor(options) {
+//     this.name = options.name,
+//     this.age = options.age,
+//     this.hasTail = options.hasTail
+//   }
+
+//   voice() {
+//     console.log('I,m animal')
+//   }
+// }
+
+// const animal = new Animal({
+//   name: 'animal',
+//   age: 5,
+//   hasTail: true
+// })
+
+// class Cat extends Animal{}
+
+// const cat = new Cat({
+//   name: 'Cat',
+//   age: 7,
+//   hasTail: true
+// })
+
+// Задание 1: ""Управление библиотекой книг""
+
+// Реализуйте класс Book, представляющий книгу, со следующими свойствами и методами:
+
+// Свойство title (название) - строка, название книги.
+// Свойство author (автор) - строка, имя автора книги.
+// Свойство pages (количество страниц) - число, количество страниц в книге.
+// Метод displayInfo() - выводит информацию о книге (название, автор и количество страниц).
+
+class Book {
+  constructor(options) {
+    this.title = options.title;
+    this.author = options.author;
+    this.pages = options.pages;
+  }
+
+  displayInfo() {
+    console.log(`    название: ${this.title}
+    автор: ${this.author}
+    кол-во страниц: ${this.pages}`);
+  }
+}
+
+const book1 = new Book({
+  title: "Вся королевская рать",
+  author: "Роберт Пэнн Уоррен",
+  pages: 540,
+});
+
+// Задание 2: ""Управление списком студентов""
+// Реализуйте класс Student, представляющий студента, со следующими свойствами и методами:
+
+// Свойство name (имя) - строка, имя студента.
+// Свойство age (возраст) - число, возраст студента.
+// Свойство grade (класс) - строка, класс, в котором учится студент.
+// Метод displayInfo() - выводит информацию о студенте (имя, возраст и класс).
+
+class Student {
+  constructor(name, age,grade) {
+    this.name = name
+    this.age = age
+    this.grade = grade
+  }
+
+  displayInfo() {
+    console.log(`    Name: ${this.name}
+    Age: ${this.age} лет
+    Grade: ${this.grade}`);
+  }
+}
+
+const student1 = new Student('John Smith',16,'10th grade')
